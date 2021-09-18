90431d0887e70f0217e2cdeacb4fc7f6a40a4c42
lujianfeng <lujianfeng@tuzhanai.com>
2021-09-18 18:40:48 +0800

    lu

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,21 +1,19 @@
-import Firebase from 'firebase';
-import 'firebase/firestore';
-import 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 
 // 1) when seeding the database you'll have to uncomment this!
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyDGqnQXia8-jnU70I1FTqL39YRfaeexx84',
+  authDomain: 'react-clone-netflix-d4a7a.firebaseapp.com',
+  projectId: 'react-clone-netflix-d4a7a',
+  storageBucket: 'react-clone-netflix-d4a7a.appspot.com',
+  messagingSenderId: '858715108872',
+  appId: '1:858715108872:web:0e93be807fd10b004dc3bd',
+  measurementId: 'G-28E6LPYQWS',
 };
 
-const firebase = Firebase.initializeApp(config);
+const firebase = initializeApp(config);
 // 2) when seeding the database you'll have to uncomment this!
 // seedDatabase(firebase);
 // 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
